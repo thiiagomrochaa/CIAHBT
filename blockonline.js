87231299fdbec6e4d5048d0922a0167f329cbafb
thiiagomrochaa <thiagomrochaa@gmail.com>
2026-08-19 19:11:07 -0300

    Fix text replacement for registered users

--- blockonline.js
+++ blockonline.js
@@ -23,7 +23,7 @@
           texto = texto.replace(/[\[\]]/g, '');
         }
         texto = texto.replace(/,/g, ' ');
-        texto = texto.replace('Usuários registrados', 'Usuários conectados');
+        texto = texto.replace('Utilizadores registados', 'Usuários conectados');
         if (texto.trim() === '') { return; }
         novosFilhos.push(document.createTextNode(texto));
         return;
